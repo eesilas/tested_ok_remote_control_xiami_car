9cfff775034b89e7b1ff596fefd2e5f5a3923fa0
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-05 10:11:26 +0800

    microbit control xiami car

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+})
 input.onGesture(Gesture.LogoUp, function () {
     radio.sendNumber(3)
     basic.showLeds(`
@@ -5,6 +15,54 @@ input.onGesture(Gesture.LogoUp, function () {
         . . # . .
         # . # . #
         . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(2)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    radio.sendNumber(9)
+    basic.showLeds(`
+        . # # . .
+        . # # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(7)
+    basic.showIcon(IconNames.Giraffe)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(6)
+    basic.showIcon(IconNames.House)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(8)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(4)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
         . . # . .
         `)
 })
@@ -18,45 +76,7 @@ input.onGesture(Gesture.LogoDown, function () {
         . . # . .
         `)
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(4)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-})
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(5)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(7)
-    basic.showIcon(IconNames.Giraffe)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(6)
-    basic.showIcon(IconNames.House)
-})
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendNumber(2)
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
-})
-radio.setGroup(12)
+radio.setGroup(13)
 radio.setTransmitPower(7)
 basic.showIcon(IconNames.Duck)
 basic.forever(function () {
